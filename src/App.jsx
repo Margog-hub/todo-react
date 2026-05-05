3288c49b1708e29463ac884074d10c443b238b5b
Marina Kilbas <margog1982@gmail.com>
2026-05-05 22:18:53 +0300

    зроблено форму керованою

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -6,20 +7,35 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
+
 function App() {
  
+const [data, setData] = useState('')
+const [pass, setPass] = useState('')
 
+const handleLogin =() => {
+  setData(Date.now())
+}
+
+const handleChangeLogin =(e)=> {
+ setData(e.target.value)
+}
+
+const handleChangePass =(e)=> {
+ setPass(e.target.value)
+}
   return (
     
     <Stack sx={{ alignItems: 'center' }}>
       <Stack sx={{ width: '400px' }} gap={2}>
 
     <Typography variant="h3" gutterBottom>
-        Вхід в сервіс
+        Вхід в сервіс {data}
       </Typography>
-    <TextField id="login" label="Логін" variant="standard" />
-    <TextField id="password" label="Пароль" variant="standard" type="password"/>
-    <Button variant="contained">Війти</Button>
+    <TextField id="login" label="Логін" variant="standard" onChange={handleChangeLogin} value ={data}/>
+    <TextField id="password" label="Пароль" variant="standard" type="password"
+    onChange={handleChangePass} value={pass}/>
+    <Button variant="contained"onChange={handleLogin} >Війти</Button>
      </Stack>
     </Stack>
   
