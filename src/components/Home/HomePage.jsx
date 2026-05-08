363666df01d662a202cc82cee36d67c64d946e3d
Marina Kilbas <margog1982@gmail.com>
2026-05-08 16:16:05 +0300

    додано видалення і зраблено/не зроблено todo

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -15,27 +15,43 @@ const todo = {
 const HomePage = (props) => {
   const [todos, setTodos] = useState([todo])
 
-  
-const handleAddTodo = (title, desc) => {
-  const newTodo = {
-    _id: Date.now().toString(),
-    title: title,
-    description: desc,
-    isDone: false
+
+  const handleAddTodo = (title, desc) => {
+    const newTodo = {
+      _id: Date.now().toString(),
+      title: title,
+      description: desc,
+      isDone: false
+    }
+    setTodos([newTodo, ...todos])
   }
-  setTodos([newTodo, ...todos])
-}
+
+
+
+  const handleDeleteTodo =(_id) => {
+   setTodos( todos.filter((todo) => { return todo._id !== _id}))
+  }
+
+
+  const handleisDoneTodo =(_id) => {
+   setTodos( todos.map((todo) => { return todo._id === _id ?
+    {...todo, isDone: !todo.isDone} : todo
+   }))
+  }
   return (
     <div>
       <Typography variant="subtitle1" gutterBottom>
         {props.username}
       </Typography>
-      <AddTodo addTodo={handleAddTodo}/>
-        {/* <Button size="small" onClick={handleAddTodo}>Додати</Button> */}
-        {
+      <AddTodo addTodo={handleAddTodo} />
+
+      {
         todos.map((todo) => (
-        <TodoItem key={todo._id} todo={todo} />
-      ))
+          <TodoItem key={todo._id} 
+          todo={todo} 
+          handleDeleteTodo={handleDeleteTodo}
+          handleisDoneTodo = {handleisDoneTodo}/>
+        ))
       }
 
     </div>
